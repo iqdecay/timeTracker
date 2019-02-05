0b748714fe557a25842f7f92fcbc7d3bf32010f6
vnepveu <victor.nepveu@imt-atlantique.net>
2019-02-05 16:29:33 +0100

    Added time tracking base function

--- ui.go
+++ ui.go
@@ -1,22 +1,59 @@
 package main
 
-import "github.com/andlabs/ui"
+import (
+	"fmt"
+	"github.com/andlabs/ui"
+	"time"
+)
+
+type Project struct {
+	name     string
+	duration time.Duration
+	id       int
+}
+
+func startTracking(b *ui.Button) {
+	//beginTime := time.Now()
+
+}
 
 func main() {
+	// Send and receive begin times for tracking
+	beginTimes := make(chan time.Time)
+	endTimes := make(chan time.Time)
+	// Send and receive projects ids
+	selectedProject := make(chan int)
+	go func() {
+		project := <-selectedProject
+		fmt.Println(project)
+	}()
+	go func() {
+		for {
+			beginTime := <-beginTimes
+			endTime := <-endTimes
+			duration := endTime.Sub(beginTime)
+			fmt.Println(duration)
+		}
+	}()
 	err := ui.Main(func() {
-		input := ui.NewEntry()
-		button := ui.NewButton("Greetings stranger")
-		greeting := ui.NewLabel("")
 		box := ui.NewVerticalBox()
-		box.Append(ui.NewLabel("Enter your name:"), false)
-		box.Append(input, false)
+		button := ui.NewButton("Play")
+		empty := ui.NewVerticalBox()
 		box.Append(button, false)
-		box.Append(greeting, false)
-		window := ui.NewWindow("Hello", 200, 100, false)
+		box.Append(empty, true)
+		window := ui.NewWindow("Hello", 400, 200, false)
 		window.SetMargined(true)
 		window.SetChild(box)
-		button.OnClicked(func(*ui.Button) {
-			greeting.SetText("Hello, " + input.Text() + "!")
+		button.OnClicked(func(b *ui.Button) {
+			if b.Text() == "Play" {
+				b.SetText("Pause")
+				beginTimes <- time.Now()
+
+			} else {
+				b.SetText("Play")
+				endTimes <- time.Now()
+			}
+
 		})
 		window.OnClosing(func(*ui.Window) bool {
 			ui.Quit()
@@ -27,4 +64,5 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 }
